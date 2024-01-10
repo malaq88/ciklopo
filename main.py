73bbeca9666948265ada4e8d633104dc17ff3e6b
Antonio Malaquias <atrito12@gmail.com>
2024-01-10 11:24:22 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import cv2
+import pytesseract
+
+# Configuração do pytesseract (ajuste conforme necessário)
+# Necessário instalação do tesseract(´sudo apt-get install tesseract-ocr´)
+pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+
+# Lista de placas
+lista_de_placas = ["ABC123", "XYZ789", "123ABC", "789XYZ", "BRA2E19"]
+
+
+def capture_and_ocr():
+    # Inicializar a câmera (0 indica a câmera padrão)
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        # Capturar frame da câmera
+        ret, frame = cap.read()
+
+        # Realizar OCR na imagem
+        text = pytesseract.image_to_string(frame)
+
+        # Exibir o texto na janela
+        print("Texto da placa:", text)
+
+        # Verificar se a placa reconhecida está na lista
+        if any(placa in text for placa in lista_de_placas):
+            print("Placa encontrada:", text)
+            break
+        else:
+            print("Placa não encontrada")
+
+        # Exibir o frame com o texto (você pode personalizar essa parte conforme necessário)
+        cv2.imshow('Camera OCR', frame)
+
+        # Encerrar o loop ao pressionar a tecla 'q'
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Liberar a câmera e fechar a janela
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    capture_and_ocr()
